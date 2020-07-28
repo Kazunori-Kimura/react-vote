de3d53057db9b969267b04580f9f945b92d76887
Kazunori Kimura <kazunori.kimura.js@gmail.com>
2020-07-28 10:29:42 +0900

    [mod] editorconfig, eslint, prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
+        'react/jsx-indent': ['error', 4],
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
+        'react/jsx-indent-props': ['error', 4],
+        'react/prop-types': 'off',
+        'react/default-props-match-prop-types': 'off',
+        'react/require-default-props': 'off',
+        'jsx-a11y/anchor-is-valid': 0,
+        'jsx-a11y/label-has-associated-control': [
+            2,
+            {
+                required: {
+                    some: ['nesting', 'id'],
+                },
+            },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+                mjs: 'never',
+            },
+        ],
+        '@typescript-eslint/no-empty-function': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        camelcase: 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+};
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # React + TypeScript による簡易アンケートアプリ
+
+React × Typescript × ESLint × Prettier × VSCodeなSetup - Qiita
+https://qiita.com/sho-t/items/c9fe6d382636bd3402f8
+
+> npx eslint --init
+
+> npm install --save-dev prettier eslint-config-prettier eslint-plugin-prettier
+
+> touch .prettierrc.js
+> touch .editorconfig
